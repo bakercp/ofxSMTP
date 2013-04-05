0506ce97629460e6307cc43d3ca96183ddf4809f
Christopher Baker <me@christopherbaker.net>
2013-04-05 01:08:25 -0500

    Unfinished logger channel.

--- src/ofxSMTPLoggerChannel.cpp
+++ src/ofxSMTPLoggerChannel.cpp
@@ -1,9 +1,28 @@
-//
-//  ofxSMTPLoggerChannel.cpp
-//  HSPB
-//
-//  Created by Christopher P. Baker on 4/2/13.
-//
-//
-
 #include "ofxSMTPLoggerChannel.h"
+
+//------------------------------------------------------------------------------
+ofSMTPLoggerChannel::ofSMTPLoggerChannel() { }
+
+//------------------------------------------------------------------------------
+ofSMTPLoggerChannel::~ofSMTPLoggerChannel() { }
+
+//------------------------------------------------------------------------------
+void ofSMTPLoggerChannel::ofSMTPLoggerChannel::log(ofLogLevel level,
+                                                   const string & module,
+                                                   const string & message) {
+    // not yet implemented
+}
+
+//------------------------------------------------------------------------------
+void ofSMTPLoggerChannel::ofSMTPLoggerChannel::log(ofLogLevel logLevel,
+                                                   const string & module,
+                                                   const char* format, ...) {
+    // not yet implemented
+}
+
+//------------------------------------------------------------------------------
+void ofSMTPLoggerChannel::ofSMTPLoggerChannel::log(ofLogLevel logLevel,
+                                                   const string & module,
+                                                   const char* format, va_list args) {
+    // not yet implemented
+}
